dabe476920ae595dd14d458762ac76af438c1dac
Víctor Manuel Herrera <vmherrera@vmherrera-macbook.local>
2023-12-23 19:31:58 +0100

    Header code process

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import logo from '../assets/images/logo.png'
+import { IoLogoYoutube } from 'react-icons/io5'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='flex justify-between items-center'>
+      <img src={logo} className='w-[180px]'/>
+      <ul className='flex gap-4 md:gap-14'>
+        <li className='hover:font-bold cursor-pointer'>Home</li>
+        <li className='hover:font-bold cursor-pointer'>About Us</li>
+        <li className='hover:font-bold cursor-pointer'>Contact Us</li>
+      </ul>
+      <button className='bg-red-500 rounded-full text-white flex items-center'>
+        Subscribe <IoLogoYoutube className='ml-3 text-[20px]'/>
+      </button>
+    </div>
   )
 }
 
